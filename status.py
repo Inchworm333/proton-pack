a8937a7245122077ad2c5afd2baa55bd03327e5b
Nicholas Valletta <inchworm333@gmail.com>
2021-10-28 00:41:20 -0400

    finished implementing the status leds w/ gpiozero

--- status.py
+++ status.py
@@ -1,8 +1,27 @@
 from enum import Enum
 import gpiozero
-import colorzero
+import threading
 
-firing = False
+class FiringStatusLeds:
 
-class FiringStatusLed:
+    def __init__(self):
+        self.leds = {}
+        self.thread = None
 
+        self.leds['green'] = LED(17)
+        self.leds['yellow'] = LED(16)
+        self.leds['red'] = LED(15)
+
+        self.all_on()
+
+    def blink(self, color, speed): 
+        self.all_on()
+        self.leds[color].blink(speed, speed)
+
+    def all_on(self):
+        for value in self.leds.items():
+            value.on()
+
+    def all_off(self):
+        for value in self.leds.items():
+            value.off()
