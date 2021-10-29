3fabe8248ae246d1a60d3fc7073f9175fd8f0dbb
Nicholas Valletta <inchworm333@gmail.com>
2021-10-29 03:32:33 -0400

    finished effects and mode changes for cyclotron

--- cyclotron.py
+++ cyclotron.py
@@ -15,24 +15,32 @@ class Cyclotron:
         
         self.thread = None
         self.color = "red"
-        self.speed = 1
+        self.speed = 0.85
         self.start_spin()
 
-    def spin_function(self, color, speed):
+    def spin_function(self):
         while self.spin:
             for led in self.leds:
-                led.color = colorzero.Color(color)
-                #led.pulse(1, 1, (1,0,0), (1,0.5,0.5))
-                time.sleep(speed)
+                led.color = colorzero.Color(self.color)
+                time.sleep(self.speed)
                 led.off()
 
-    def start_spin(self):
+    def spin_fade_function(self):
+        while self.spin:
+            for led in self.leds:
+                led.pulse(self.speed/2, self.speed/2, colorzero.Color(self.color))
+                time.sleep(self.speed)
+                led.off()
+
+    def start_spin(self, spin_func):
         self.spin = True
-        self.thread = threading.Thread(target=self.spin_function, args=(self.color, self.speed))
+        self.thread = threading.Thread(target=spin_func)
         self.thread.start()
 
     def stop_spin(self):
         self.spin = False
+        for led in self.leds:
+            led.off()
 
     def mode():
         global mode
@@ -41,12 +49,20 @@ class Cyclotron:
 
         if mode_decoded == "proton":
             self.color = "red"
+            self.stop_spin()
+            self.start_spin(self.spin_function)
         elif mode_decoded == "slime":
             self.color = "green"
+            self.stop_spin()
+            self.slime_bubble_start()
         elif mode_decoded == "stasis":
             self.color = "blue"
+            self.stop_spin()
+            self.start_spin(self.spin_fade_function)
         elif mode_decoded == "meson":
             self.color = "yellow"
+            self.stop_spin()
+            self.start_spin(self.spin_function)
 
     def slime_bubble_function(self, led):
         led.pulse(random.uniform(0.3, 1.8), 0, (0.05, random.uniform(0.73, 0.97), 0.08), (0.05, random.uniform(0.48, 0.67), 0.08))
