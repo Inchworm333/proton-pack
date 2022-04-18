5475b5ed23d8198e510de567e0a1d7e098e8a200
Nicholas Valletta <inchworm333@gmail.com>
2022-04-18 08:00:16 +0100

    Working more on proton pack state framework.

--- modes/pack.py
+++ modes/pack.py
@@ -15,3 +15,24 @@ class Pack:
 
     def currentState(self):
         print(f"The proton pack is in {type(self._state).__name__} mode")
+
+    def arm(self):
+        self._state.arm()
+
+    def disarm(self):
+        self._state.disarm()
+
+    def fire(self):
+        self._state.fire()
+
+    def ceasefire(self):
+        self._state.ceasefire()
+
+
+    def exitState(self):
+        self._state.exitState()
+
+    def  packOff(self):
+        self._state.packOff()
+
+    #TODO check if mode stays same after disarm
