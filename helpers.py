bb292864f4e9532ac61eecab11ee4d5dc2bd8df9
Nicholas Valletta <inchworm333@gmail.com>
2025-12-27 01:56:48 -0500

    Changed modes to an Enum

--- helpers.py
+++ helpers.py
@@ -3,25 +3,19 @@ import random
 import gpiozero
 import colorzero
 import gpiozero.threads
+from enum import Enum
 
 #helpers.py
 #has helper functions that might be needed in more than one file.
 
+class Mode(Enum):
+    PROTON = 0
+    SLIME = 1
+    STASIS = 2
+    MESON = 3
+
 def mode_decode(modeID):
-    inner_mode = modeID % 4
-    
-    if inner_mode == 0:
-        #Proton
-        return "proton"
-    elif inner_mode == 1:
-        #Slime
-        return "slime"
-    elif inner_mode == 2:
-        #Stasis
-        return "stasis"
-    elif inner_mode == 3:
-        #Meson
-        return "meson"
+    return Mode(modeID % 4).name.lower()
 
 def play_wait(sound, delay=0):
     sound.play()
